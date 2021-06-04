7d187e1377a8271b50cdc5c1acb600d4b8b5f480
xeyong <recruyong@gmail.com>
2021-06-04 17:38:22 +0900

    type: rest api test initial seyong

---
+++ src/main/java/com/example/helloapi/HelloapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloapiApplication.class, args);
+    }
+
+}
